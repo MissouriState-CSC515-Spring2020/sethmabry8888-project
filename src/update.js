5893a4436459819f29fe6054d682800c3c95fadf
Seth Mabry <mabry888@live.missouristate.edu>
2020-01-31 15:44:07 -0600

    Final commit

--- src/update.js
+++ src/update.js
@@ -1,8 +1,10 @@
 import format from 'date-fns/format';
 
 var span = document.querySelector('#time-now');
+var hello = document.querySelector('#hello');
 
 export default function update() {
+	hello.textContent = "Hello World";
 	span.textContent = format(new Date(), 'h:mm:ssa');
 	setTimeout(update, 1000);
 }
